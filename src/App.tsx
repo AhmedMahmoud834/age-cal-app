291ece3481e78410ee0d217ea44ac98428298f69
AhmedMahmoud834 <am7052302@gmail.com>
2026-05-25 17:24:06 +0300

    removed padding for main tag

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <main className="bg-[hsl(0,0%,94%)] min-h-screen m-0 pt-12 pb-16 px-4">
+      <main className="bg-[hsl(0,0%,94%)] min-h-screen m-0 pt-12 px-4">
         <div className="bg-white rounded-3xl rounded-br-[100px] p-6 test flex flex-col gap-12 max-w-4xl mx-auto md:p-14 ">
           <div>
             <AgeForm setResult={setResult} />
